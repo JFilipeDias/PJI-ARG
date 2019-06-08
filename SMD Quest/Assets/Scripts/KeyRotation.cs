833b7f59a78e4c7427fef00022d2c1507748869b
Filipe Dias <filipediascontato@gmail.com>
2019-06-08 20:08:48 -0300

    Mudança no script KeyRotation

---
+++ SMD Quest/Assets/Scripts/KeyRotation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRotation : MonoBehaviour
+{
+    private Quaternion originalRotation;
+    private float startAngle = 0;
+    private int keyMask;
+
+    public void Start()
+    {
+        keyMask = LayerMask.GetMask("KeyMask");
+        originalRotation = this.transform.rotation;
+    }
+
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            HandleTouch();
+            //Debug.Log("Tocou");
+        }
+    }
+
+
+    private void HandleTouch()
+    {
+        Touch playerTouch = Input.GetTouch(0);
+
+        Ray cameraRay = Camera.main.ScreenPointToRay(playerTouch.position);
+        RaycastHit rayHit;
+
+        Vector2 keyScreenPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+        Vector2 vector = Input.GetTouch(0).position - keyScreenPosition;
+
+        //if (Physics.Raycast(cameraRay, out rayHit, Mathf.Infinity, keyMask))
+        //{
+            //Debug.Log("Tocou na chave");
+            if (playerTouch.phase == TouchPhase.Began)
+            {
+                originalRotation = this.transform.rotation;
+                startAngle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+            }
+
+            if (playerTouch.phase == TouchPhase.Moved)
+            {
+                float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+                Quaternion newRotation = Quaternion.AngleAxis(angle - startAngle, this.transform.forward);
+                newRotation.y = 0;
+                newRotation.eulerAngles = new Vector3(0, 0, newRotation.eulerAngles.z);
+                this.transform.rotation = originalRotation * newRotation;
+            }
+        //}
+    }
+}
--- SMD Quest/Assets/Scripts/Rotation.cs
+++ SMD Quest/Assets/Scripts/Rotation.cs
@@ -4,39 +4,5 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    private Quaternion originalRotation;
-    private float startAngle = 0;
-
-
-    public void Start()
-    {
-        originalRotation = this.transform.rotation;
-    }
-
-
-    private void Update()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch playerTouch = Input.GetTouch(0);
-
-            Vector2 keyScreenPosition = Camera.main.WorldToScreenPoint(this.transform.position);
-            Vector2 vector = Input.GetTouch(0).position - keyScreenPosition;
-
-            if (playerTouch.phase == TouchPhase.Began)
-            {
-                originalRotation = this.transform.rotation;
-                startAngle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
-            }
-
-            if (playerTouch.phase == TouchPhase.Moved)
-            {
-                float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
-                Quaternion newRotation = Quaternion.AngleAxis(angle - startAngle, this.transform.forward);
-                newRotation.y = 0; 
-                newRotation.eulerAngles = new Vector3(0, 0, newRotation.eulerAngles.z);
-                this.transform.rotation = originalRotation * newRotation;
-            }
-        }
-    }
+    
 }
