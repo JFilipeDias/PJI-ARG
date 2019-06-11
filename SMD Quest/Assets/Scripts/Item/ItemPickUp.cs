0a13e34fdc3836e670a1604fe8830b719fcd4979
Filipe Dias <filipediascontato@gmail.com>
2019-06-11 10:39:22 -0300

    Bug fix do inventario
    Agora funciona normalmente

--- SMD Quest/Assets/Scripts/Item/ItemPickUp.cs
+++ SMD Quest/Assets/Scripts/Item/ItemPickUp.cs
@@ -5,40 +5,11 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
     public Item item;
-    private Transform itemTrasnform;
-    private int itemMask;
-
-    private void Awake()
-    {
-        itemMask = LayerMask.GetMask("ItemMask");
-    }
-
-    private void Update()
-    {
-        if (Input.touchCount > 0)
-        {
-            CheckTouch();
-        }
-    }
-
-
-    private void CheckTouch()
-    {
-        Touch playerTouch = Input.GetTouch(0);
-        Ray camRay = Camera.main.ScreenPointToRay(playerTouch.position);
-        RaycastHit rayHit;
-
-        if (Physics.Raycast(camRay, out rayHit, Mathf.Infinity, itemMask))
-        {
-            itemTrasnform = rayHit.transform;
-            itemTrasnform.GetComponent<ItemPickUp>().PickUp();
-        }
-    }
 
 
     public void PickUp()
     {
-        bool wasPickedUp = Inventory.instance.Add(itemTrasnform.GetComponent<ItemPickUp>().item);
+        bool wasPickedUp = Inventory.instance.Add(item);
 
         if (wasPickedUp)
             Destroy(this.gameObject);
---
+++ SMD Quest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            CheckTouch();
+        }
+    }
+
+
+    private void CheckTouch()
+    {
+        Touch playerTouch = Input.GetTouch(0);
+        Ray camRay = Camera.main.ScreenPointToRay(playerTouch.position);
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(camRay, out rayHit, Mathf.Infinity, LayerMask.GetMask("ItemMask")))
+        {
+            rayHit.transform.GetComponent<ItemPickUp>().PickUp();
+        }
+    }
+}
